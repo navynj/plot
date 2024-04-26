f8a99a1fb6aa945f987b3de10203a2d9266120d7
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-04-26 17:09:53 +0900

    feat: Category, Subject | Create route & store to fetch data #5

---
+++ app/api/subject/route.ts
@@ -0,0 +1,22 @@
+import { auth } from '@/lib/auth';
+import { prisma } from '@/prisma/client';
+
+export async function GET(req: Request) {
+  const session = await auth();
+
+  if (!session) {
+    return new Response('Session not found', {
+      status: 401,
+    });
+  }
+
+  try {
+    const data = await prisma.subject.findMany({
+      where: { userId: session.user.id },
+    });
+
+    return new Response(JSON.stringify(data), { status: 201 });
+  } catch (error) {
+    return new Response('Failed to fetch', { status: 500 });
+  }
+}
