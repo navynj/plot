6ed843368bda20b2ecfd4bf3a3c8b9936b7c02fa
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-04-30 12:32:08 +0900

    feat: DB | Add isPlan, repeatingDays, repeatingDates field & Update todo findMany query #7

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -12,22 +12,75 @@ export async function GET(req: NextRequest) {
   }
 
   const searchParams = req.nextUrl.searchParams;
-  const date = searchParams.get('date');
+  const dateStr = searchParams.get('date');
 
-  if (!date) {
+  if (!dateStr) {
     return new Response('Date query is empty', {
       status: 400,
     });
   }
 
+  const date = new Date(dateStr);
+
   try {
     const data = await prisma.todo.findMany({
-      where: { userId: session.user.id, date: new Date(date) },
-      orderBy: { createdAt: 'asc' },
+      where: {
+        userId: session.user.id,
+        NOT: { excludeDates: { has: date } },
+        OR: [
+          { date },
+          { repeatingDays: { has: date.getDay() } },
+          { repeatingDates: { has: date.getDate() } },
+          {
+            AND: [
+              { isRepeating: true },
+              {
+                OR: [
+                  {
+                    AND: [{ repeatingStart: undefined }, { repeatingEnd: undefined }],
+                  },
+                  {
+                    AND: [
+                      { repeatingStart: { gte: date } },
+                      { repeatingEnd: { lte: date } },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      orderBy: { rank: 'asc' },
     });
 
     return new Response(JSON.stringify(data), { status: 201 });
   } catch (error) {
+    console.log(error);
     return new Response('Failed to fetch', { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  const session = await auth();
+
+  if (!session) {
+    return new Response('Session not found', {
+      status: 401,
+    });
+  }
+
+  const { title } = await req.json();
+  try {
+    const data = await prisma.todo.create({
+      data: {
+        title,
+        userId: session.user.id,
+      },
+    });
+
+    return new Response(JSON.stringify(data), { status: 201 });
+  } catch (error) {
+    return new Response('Failed to create todo', { status: 500 });
   }
 }
